9fce022ff91ad3693b602a477e0db522ebb8c2da
zhenpanda <zhentzhou@gmail.com>
2016-09-04 20:21:38 -0400

    yarwnnn

--- unsolved/commonDenom.js
+++ unsolved/commonDenom.js
@@ -17,47 +17,49 @@ in which D is as small as possible and
 
 */
 
-
 function convertFrac(lst){
-
-  //get the denominators
-
-
-
-}
-convertFrac([ [1, 2], [1, 3], [1, 4] ])
-
-//find lcd
-function lcd(){
-  // get arguments into an array
-  var args = [].slice.call(arguments);
-  // find gcm of all nums in array
-  var gcm = args.reduce((a,b)=>{return a*b})
-  var m = 1
-  var mAry = []
-  // find all mulitples of the gcm
-  while (m++ < gcm) {
-    if (gcm % m === 0) {
-      mAry.push(m)
-    }
-  }
-  //console.log(mAry);
-  // find all the numbers in the gcm array that is not a common mulitple in arguments ary
-  var filtered =  mAry.filter((d)=>{
-    for (let a in args) {
-      if (d % args[a] !== 0) {
-        //console.log(d);
-        return d
+  // find lcd
+  function lcd(inputAry){
+    // var args = [].slice.call(this, arguments);
+    var gcm = inputAry.reduce((a,b)=>{return a*b})
+    var m = 1
+    var mAry = []
+    // find all mulitples of the gcm
+    while (m++ < gcm) {
+      if (gcm % m === 0) {
+        mAry.push(m)
       }
     }
-  })
-  // find all the numbers that can lcm
-  var cm = mAry.filter((c)=>{
-    return filtered.indexOf(c) == -1;
-  })
-  // return small common mulitple
-  //console.log(cm)
-  console.log( cm[0] )
+    // console.log(mAry)
+    // find all the numbers in the gcm array that is not a common mulitple in arguments ary
+    var l = []
+    for (let d = 0; d < mAry.length; d++){
+      // console.log(mAry[d])
+      let c = 0
+      for (let a = 0; a < inputAry.length; a++) {
+        // console.log(inputAry[a],"%",mAry[d],inputAry[a] % mAry[d])
+        if (mAry[d] % inputAry[a] === 0) {
+          // console.log(mAry[d],"%",inputAry[a], mAry[d] % inputAry[a])
+          c++
+        }
+        if (c === inputAry.length) {
+          l.push(mAry[d])
+        }
+      }
+    }
+    return l[0]
+  }
+  // get denominators
+  var denom = []
+  for (let e = 0; e < lst.length; e++) {
+    denom.push(lst[e][1])
+  }
+  var d = lcd(denom)
+  var str = ""
+  for (let s = 0; s < lst.length; s++) {
+    str += "(" + (d/lst[s][1]) + "," + d + ")"
+  }
+  return str
 }
-lcd(2,3,4)
-lcd(5,15,25)
+convertFrac([ [1, 2], [1, 3], [1, 4] ])
+// convertFrac([ [ 69, 130 ], [ 87, 1310 ], [ 3, 4 ] ])
