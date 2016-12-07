1e37966a4e5e27aaf2de8eac46943650094b11d6
Zhenting <zhentzhou@gmail.com>
2016-12-06 22:11:48 -0500

    piece of cake

---
+++ js/cakes.js
@@ -0,0 +1,36 @@
+/*
+
+Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+// must return 2
+cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+
+// must return 0
+cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+
+*/
+
+function cakes(recipe, available) {
+  let total = [];
+  Object.keys(recipe).forEach((k,v)=>{
+    let amount;
+    if (available[k]) {
+      amount = parseInt(available[k]/recipe[k]);
+    }else {
+      amount = 0;
+    }
+    total.push(amount);
+  });
+  let out=total[0];
+  // console.log(total)
+  total.map((e)=>{
+    if (e<=out) out = e;
+  })
+  return out;
+}
+
+recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
+available = {sugar: 500, flour: 2000, milk: 2000};
+cakes(recipe, available) // 2
