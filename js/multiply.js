890e6ea2da59d358a4ef7f651fc3f7f338b2ccf7
Zhenting <zhenpanda@users.noreply.github.com>
2020-03-08 10:08:37 -0400

    crazy math shet

---
+++ js/multiply.js
@@ -0,0 +1,51 @@
+function multiply(a, b) {
+    function mult(a, b) {
+        if (a.length < 5 && b.length < 5) {
+            return (parseInt(a) * parseInt(b)).toString();
+        }
+    
+        a = a.split('').reverse();
+        b = b.split('').reverse();
+        var result = [];
+    
+        for (var i = 0; a[i] >= 0; i++) {
+            for (var j = 0; b[j] >= 0; j++) {
+                if (!result[i + j]) {
+                    result[i + j] = 0;
+                }
+    
+                result[i + j] += a[i] * b[j];
+            }
+        }
+    
+        for (i = 0; result[i] >= 0; i++) {
+            if (result[i] >= 10) {
+                if (!result[i + 1]) {
+                    result[i + 1] = 0;
+                }
+    
+                result[i + 1] += parseInt(result[i] / 10);
+                result[i] %= 10;
+            }
+        }
+    
+        result.reverse();
+        for (i = 0; i < result.length; i++){
+          if (result[i] === 0){
+             result.splice(i--, 1);
+          }else{
+            i = result.length * 2;
+          }
+        }
+    
+        return result.join('');
+    }
+    const num = mult(a,b);
+    const numParts = (num+"").split("e+");
+    if(numParts.length > 1) {
+        const size = parseInt(numParts[1]) + 1;
+        return (num.toPrecision(size))
+    }else{
+        return num+""
+    }
+}
